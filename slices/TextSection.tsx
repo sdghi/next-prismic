5ac804cb4fbe9561b65aa3539f195df3b1eb00a7
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-10-27 15:15:14 -1000

    edit: adds autoprefixer for grid comment on top of global scss

--- slices/TextSection.tsx
+++ slices/TextSection.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { RichText } from 'prismic-reactjs';
+import '../styles/slices/text-section.scss';
 
 const TextSection = ({ data }) => {
     return (
-        <section>
+        <section className="text-section">
             <div className="container">{RichText.render(data.primary.content)}</div>
         </section>
     );
