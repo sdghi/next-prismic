ba761222046e12a04f1d5469ef0bf40d311e0ed4
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-02 01:43:57 -1000

    edit: adds a useEffect to check if there is navigation data before rendering the nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,28 +4,36 @@ import NavigationPrimaryItem from './NavigationPrimaryItem';
 import NavigationMultiLinkItem from './NavigationMultiLinkItem';
 
 export default function Navigation({ navigation }) {
-    const navigationItems = navigation.data.body;
+    const [navigationItems, setNavigationItems] = useState(null);
+
+    useEffect(() => {
+        if (navigation.data) {
+            setNavigationItems(navigation.data.body);
+        }
+    }, [navigation]);
 
     return (
         <nav>
             <div className="container nav__nav-container">
                 <h1 className="nav__site-branding">{siteInfo.siteName}</h1>
-                <ul className="nav__nav-items" data-testid="nav-items-container">
-                    {navigationItems.map((item, index) => {
-                        const res = (() => {
-                            switch (item.slice_type) {
-                                case 'primary_item':
-                                    return <NavigationPrimaryItem key={index} data={item} />;
-                                case 'multi-link_item':
-                                    return <NavigationMultiLinkItem key={index} data={item} />;
-                                default:
-                                    return null;
-                            }
-                        })();
+                {navigationItems && (
+                    <ul className="nav__nav-items" data-testid="nav-items-container">
+                        {navigationItems.map((item, index) => {
+                            const res = (() => {
+                                switch (item.slice_type) {
+                                    case 'primary_item':
+                                        return <NavigationPrimaryItem key={index} data={item} />;
+                                    case 'multi-link_item':
+                                        return <NavigationMultiLinkItem key={index} data={item} />;
+                                    default:
+                                        return null;
+                                }
+                            })();
 
-                        return res;
-                    })}
-                </ul>
+                            return res;
+                        })}
+                    </ul>
+                )}
             </div>
         </nav>
     );
