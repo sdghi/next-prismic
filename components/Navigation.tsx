14b6b7e471345d82f81789901c72ad33613bb9f1
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-02 01:33:03 -1000

    feat: removes api routes and fetches navigation data in each route and passes it to _app through pageProps

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,30 +3,15 @@ import { siteInfo } from '../siteInfo';
 import NavigationPrimaryItem from './NavigationPrimaryItem';
 import NavigationMultiLinkItem from './NavigationMultiLinkItem';
 
-export default function Navigation() {
-    const [navItems, setNavItems] = useState([]);
-
-    const isProduction = process.env.NODE_ENV === 'production';
-    const server = isProduction ? 'sitename-url' : 'http://localhost:3000';
-
-    async function getNavItems() {
-        const res = await fetch(`${server}/api/navigation`);
-        const result = await res.json();
-        setNavItems(result);
-    }
-
-    useEffect(() => {
-        getNavItems();
-    }, []);
-
-    if (!navItems) return null;
+export default function Navigation({ navigation }) {
+    const navigationItems = navigation.data.body;
 
     return (
         <nav>
             <div className="container nav__nav-container">
                 <h1 className="nav__site-branding">{siteInfo.siteName}</h1>
                 <ul className="nav__nav-items" data-testid="nav-items-container">
-                    {navItems.map((item, index) => {
+                    {navigationItems.map((item, index) => {
                         const res = (() => {
                             switch (item.slice_type) {
                                 case 'primary_item':
