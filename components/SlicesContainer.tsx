87104e34788f220ca35e4670149ff30b90b78a42
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-01 20:00:20 -1000

    feat: adds the slices to the slices container switch statement

--- components/SlicesContainer.tsx
+++ components/SlicesContainer.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import TextSection from '../slices/TextSection';
 import HeroImage from '../slices/HeroImage';
 import Notification from '../slices/Notification';
+import Accordian from '../slices/Accordian';
+import TabbedContent from '../slices/TabbedContent';
+import Table from '../slices/Table';
+import Carousel from '../slices/Carousel';
 
 export default function SlicesContainer({ slices }) {
     return (
@@ -15,6 +19,14 @@ export default function SlicesContainer({ slices }) {
                             return <Notification key={index} data={slice} />;
                         case 'text_section':
                             return <TextSection key={index} data={slice} />;
+                        case 'table':
+                            return <Table key={index} data={slice} />;
+                        case 'accordian':
+                            return <Accordian key={index} data={slice} />;
+                        case 'tabbed_content':
+                            return <TabbedContent key={index} data={slice} />;
+                        case 'carousel':
+                            return <Carousel key={index} data={slice} />;
                         default:
                             return null;
                     }
