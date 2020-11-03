511e30904b5405f6b497b95feb7d457ff2c29ca0
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-03 02:59:47 -1000

    edit: creates Header component and moves navigation inside

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { siteInfo } from '../siteInfo';
 import NavigationPrimaryItem from './NavigationPrimaryItem';
 import NavigationMultiLinkItem from './NavigationMultiLinkItem';
-import Link from 'next/link';
 
 export default function Navigation({ navigation }) {
     const [navigationItems, setNavigationItems] = useState(null);
@@ -15,31 +13,24 @@ export default function Navigation({ navigation }) {
 
     return (
         <nav>
-            <div className="container nav__nav-container">
-                <h1 className="nav__site-branding">
-                    <Link href="/">
-                        <a>{siteInfo.siteName}</a>
-                    </Link>
-                </h1>
-                {navigationItems && (
-                    <ul className="nav__nav-items" data-testid="nav-items-container">
-                        {navigationItems.map((item, index) => {
-                            const res = (() => {
-                                switch (item.slice_type) {
-                                    case 'primary_item':
-                                        return <NavigationPrimaryItem key={index} data={item} />;
-                                    case 'multi-link_item':
-                                        return <NavigationMultiLinkItem key={index} data={item} />;
-                                    default:
-                                        return null;
-                                }
-                            })();
+            {navigationItems && (
+                <ul className="nav__nav-items" data-testid="nav-items-container">
+                    {navigationItems.map((item, index) => {
+                        const res = (() => {
+                            switch (item.slice_type) {
+                                case 'primary_item':
+                                    return <NavigationPrimaryItem key={index} data={item} />;
+                                case 'multi-link_item':
+                                    return <NavigationMultiLinkItem key={index} data={item} />;
+                                default:
+                                    return null;
+                            }
+                        })();
 
-                            return res;
-                        })}
-                    </ul>
-                )}
-            </div>
+                        return res;
+                    })}
+                </ul>
+            )}
         </nav>
     );
 }
