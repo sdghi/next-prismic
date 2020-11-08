5de248b92f45a669b3e70894c0bfd141ed499c06
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-08 00:28:19 -1000

    fix: Link components receives the entire link rather than just the href value

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import { linkResolver } from '../utils';
 
-export default function ResolvedLink({ href, children }) {
+export default function ResolvedLink({ link, children }) {
     return (
-        <Link as={linkResolver(href)} href={linkResolver(href)}>
+        <Link as={linkResolver(link)} href={linkResolver(link)}>
             {children}
         </Link>
     );
