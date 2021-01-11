a5e30516004fa042b8d6124b0e04fc0e0c8cc367
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-01-10 23:48:16 -1000

    feat: adds html serializer and RichText component

---
+++ components/RichText.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { RichText } from 'prismic-reactjs';
+import { htmlSerializer, linkResolver } from '@/utils/index';
+
+export default function PrismicRichText({ render }) {
+    return <RichText render={render} linkResolver={linkResolver} htmlSerializer={htmlSerializer} />;
+}
