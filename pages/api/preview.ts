9d78893f32f419c68cc1f28cd8d35649ad4b519f
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-03-21 00:11:02 -1000

    feat: preview api routes working in prod just need to enable slice machine

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,6 +1,23 @@
 import { NextApiResponse } from 'next';
+import { client } from '@/utils/prismic-configuration';
+import { linkResolver } from '@/utils/linkResolver';
 
-export default (req, res: NextApiResponse) => {
-    res.setPreviewData({});
-    res.redirect(req.query.route);
+export default async (req, res: NextApiResponse) => {
+    const { token: ref, documentId } = req.query;
+
+    // Check the token parameter agains the Prismic SDK
+    const url = await client.getPreviewResolver(ref, documentId).resolve(linkResolver, '/');
+
+    if (!url) {
+        return res.status(404).json({ message: 'Invalid Token' });
+    }
+
+    // Enable preivew mode by seetin the cookies
+    res.setPreviewData({
+        ref // pass the ref to pages so that they can fetch the draft ref
+    });
+
+    res.writeHead(301, { Location: `/` });
+
+    res.end();
 };
