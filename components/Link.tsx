b29103436aac54225c1c27a87593e0b9087d2757
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-05-16 16:43:24 -1000

    edit: link resolver and  component can support web links with target _blank

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,6 +2,13 @@ import Link from 'next/link';
 import { linkResolver } from '../utils';
 
 export default function ResolvedLink({ link, children }) {
+    if (link.link_type === 'Web')
+        return (
+            <a href={link.url} target={link.target ? link.target : 'self'} rel="noreferrer">
+                {children}
+            </a>
+        );
+
     return (
         <Link as={linkResolver(link)} href={linkResolver(link)}>
             {children}
--- utils/linkResolver.ts
+++ utils/linkResolver.ts
@@ -1,3 +1,6 @@
 export const linkResolver = (doc) => {
+    if (doc.link_type === 'Web') {
+        return doc.url;
+    }
     return `/${doc.uid}`;
 };
